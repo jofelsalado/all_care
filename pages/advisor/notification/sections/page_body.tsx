6154ece9be4a0a3d2058598c6cc9e43dac328745
mae <mmabilog@geniihut.com>
2023-04-17 11:21:08 +0800

    Mae

--- pages/advisor/notification/sections/page_body.tsx
+++ pages/advisor/notification/sections/page_body.tsx
@@ -4,18 +4,18 @@ import { RiDeleteBinLine } from "react-icons/ri";
 import { NotificationTiles } from "../components/notification_tiles";
 
 export function PageBody({ type, name }: any) {
-    return (
-        <div className="bg-white w-full h-[25rem] rounded-md ">
-            <button className=" bg-slate-200 w-full h-[3rem]  rounded-t-md ">Clear Notifcations</button>
-            <div className="p-10 flex flex-col gap-5 w-full h-[22rem] overflow-auto">
-                <NotificationTiles name="1" type="LEADS" />
-                <NotificationTiles name="2" type="LEADS" />
-                <NotificationTiles name="3" type="LEADS" />
-                <NotificationTiles name="4" type="LEADS" />
-                <NotificationTiles name="5" type="LEADS" />
-
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="bg-white w-full h-[25rem] rounded-md ">
+      <button className=" bg-slate-200 w-full h-[3rem]  rounded-t-md ">
+        Clear Notifcations
+      </button>
+      <div className="p-10 flex flex-col gap-5 w-full h-[22rem] overflow-auto">
+        <NotificationTiles name="1" type="LEADS" />
+        <NotificationTiles name="2" type="LEADS" />
+        <NotificationTiles name="3" type="LEADS" />
+        <NotificationTiles name="4" type="LEADS" />
+        <NotificationTiles name="5" type="LEADS" />
+      </div>
+    </div>
+  );
 }
